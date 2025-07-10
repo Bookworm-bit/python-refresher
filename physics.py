d1b7dc399497b1917fe4b62d8cd99e0b7fe43e51
Eric Zhang <ezhang7708@gmail.com>
2025-07-10 15:54:04 -0400

    more physics, onto p10 :(((

--- physics.py
+++ physics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import degrees, radians
 
 
 def calculate_buoyancy(V, density_fluid):
@@ -78,7 +79,7 @@ def calculate_torque(F_magnitude, F_direction, r):
     - r: distance of the object from the axis of rotation
     """
 
-    return F_magnitude * r * np.sin(F_direction * np.pi / 180)
+    return F_magnitude * r * np.sin(radians(F_direction))
 
 
 def calculate_moment_of_inertia(m, r):
@@ -94,16 +95,81 @@ def calculate_moment_of_inertia(m, r):
     return m * (r**2)
 
 
-def calculate_auv_acceleration(
-    F_magnitude, F_angle, mass=100, volume=0.1, thruster_distance=0.5
-):
+def calculate_auv_acceleration(F_magnitude, F_angle, mass=100):
     """
-    Returns the acceleration in radians per second squared of an AUV with a 100N thruster
+    Returns the acceleration in meters per second squared of an AUV with a thruster that can rotate +- 30 degrees
 
     Params
-    - F_magnitude: Newtons, magnitude of the force applied to the AUV
-    - F_angle: radians, the direction of the force applied to the AUV
+    - F_magnitude: Newtons, magnitude of the force applied by the thruster
+    - F_angle: radians, the direction of the force applied by the thruster
     - mass: kg, mass of the AUV, default is 100kg
-    - volume: m^3, volume of the AUV, default is 0.1m^3
-    - thruster_distance: meters, distance from the center of mass of the AUV to thruster, default is 0.5m
     """
+
+    return -F_magnitude * np.array([np.cos(F_angle) / mass, np.sin(F_angle) / mass])
+
+
+def calculate_auv_angular_acceleration(
+    F_magnitude, F_angle, inertia=1, thruster_distance=0.5
+):
+    """
+    Returns the angular acceleration in radians per second sqaured of an AUV with a thruster that can rotate +- 30 degrees
+
+    Params
+    - F_magnitude: Newtons, the magnitude of the force applied by the thruster
+    - F_angle: radians, the direction of the force applied by the thruster
+    - inertia: kg*m^2, the moment of inertia, default is 1 kg*m^2
+    - thruster_distance: meters, the distance fom the thruster to the center of mass, default is 0.5m
+    """
+
+    return calculate_angular_acceleration(
+        calculate_torque(F_magnitude, degrees(F_angle), thruster_distance), inertia
+    )
+
+
+def calculate_auv2_acceleration(T, alpha, theta, mass=100):
+    """
+    Returns the acceleration in meters per second squared of the 4 thruster AUV
+
+    Params
+    - T: np.ndarray of Newtons, magnitudes of the forces applied by the thrusters
+    - alpha: radians, angle of the thrusters
+    - theta: radians, the angle of the AUV
+    - mass: kg, the mass of the AUV, default is 100kg
+    """
+
+    T1 = theta + alpha
+    T2 = theta - alpha
+    T3 = theta + np.pi + alpha
+    T4 = theta + np.pi - alpha
+
+    acceleration1 = calculate_auv_acceleration(T[0], T1, mass)
+    acceleration2 = calculate_auv_acceleration(T[1], T2, mass)
+    acceleration3 = calculate_auv_acceleration(T[2], T3, mass)
+    acceleration4 = calculate_auv_acceleration(T[3], T4, mass)
+
+    return acceleration1 + acceleration2 + acceleration3 + acceleration4
+
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia=100):
+    """
+    Returns the angular acceleration in radians per second squared of the 4 thruster AUV
+
+    Params
+    - T: np.ndarray of Newtons, magnitudes of the forces applied by the thrusters
+    - alpha: radians, angle of the thrusters
+    - L: distance from the center of mass to the thruster
+    - l: duplicate of L
+    - inertia: kg*m^2, moment of inertia of the AUV, default is 100 kg*m^2
+    """
+
+    T1 = alpha
+    T2 = -alpha
+    T3 = np.pi + alpha
+    T4 = np.pi - alpha
+
+    acceleration1 = calculate_auv_angular_acceleration(T[0], T1, inertia, L)
+    acceleration2 = calculate_auv_angular_acceleration(T[1], T2, inertia, L)
+    acceleration3 = calculate_auv_angular_acceleration(T[2], T3, inertia, L)
+    acceleration4 = calculate_auv_angular_acceleration(T[3], T4, inertia, L)
+
+    return acceleration1 + acceleration2 + acceleration3 + acceleration4
