6d76388c54b578b62c76921f36258827a7e34df0
Eric Zhang <ezhang7708@gmail.com>
2025-07-14 13:48:04 -0400

    did some physics, simulation is not very good

--- physics.py
+++ physics.py
@@ -1,5 +1,11 @@
 import numpy as np
-from math import degrees, radians
+import matplotlib
+
+# from math import degrees, radians, sqrt
+
+GRAVITATIONAL_ACC = 9.81
+WATER_DENSITY = 1000  # kg/m^3
+ATMOSPHERIC_PRESSURE = 101325  # Pa
 
 
 def calculate_buoyancy(V, density_fluid):
@@ -12,7 +18,7 @@ def calculate_buoyancy(V, density_fluid):
     - density_fluid: kg/m^3, density of the fluid
     """
 
-    return density_fluid * V * 9.81
+    return density_fluid * V * GRAVITATIONAL_ACC
 
 
 def will_it_float(V, mass):
@@ -26,7 +32,7 @@ def will_it_float(V, mass):
     Assuming that the density of water is 1000 kg/m^3
     """
 
-    return mass / V >= 1
+    return mass / V <= 1000
 
 
 def calculate_pressure(depth):
@@ -38,7 +44,14 @@ def calculate_pressure(depth):
     - depth: meters, depth in the water
     """
 
-    return 1000 * 9.81 * depth
+    if depth < 0:
+        raise (ValueError("Depth must be positive!"))
+
+    return 1000 * GRAVITATIONAL_ACC * depth + ATMOSPHERIC_PRESSURE
+
+
+def calculate_depth(pressure):
+    return (pressure - ATMOSPHERIC_PRESSURE) / (1000 * GRAVITATIONAL_ACC)
 
 
 def calculate_acceleration(F, m):
@@ -79,7 +92,7 @@ def calculate_torque(F_magnitude, F_direction, r):
     - r: distance of the object from the axis of rotation
     """
 
-    return F_magnitude * r * np.sin(radians(F_direction))
+    return F_magnitude * r * np.sin(np.radians(F_direction))
 
 
 def calculate_moment_of_inertia(m, r):
@@ -105,7 +118,7 @@ def calculate_auv_acceleration(F_magnitude, F_angle, mass=100):
     - mass: kg, mass of the AUV, default is 100kg
     """
 
-    return -F_magnitude * np.array([np.cos(F_angle) / mass, np.sin(F_angle) / mass])
+    return F_magnitude * np.array([np.cos(F_angle) / mass, np.sin(F_angle) / mass])
 
 
 def calculate_auv_angular_acceleration(
@@ -122,7 +135,7 @@ def calculate_auv_angular_acceleration(
     """
 
     return calculate_angular_acceleration(
-        calculate_torque(F_magnitude, degrees(F_angle), thruster_distance), inertia
+        calculate_torque(F_magnitude, np.degrees(F_angle), thruster_distance), inertia
     )
 
 
@@ -167,9 +180,59 @@ def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia=100):
     T3 = np.pi + alpha
     T4 = np.pi - alpha
 
-    acceleration1 = calculate_auv_angular_acceleration(T[0], T1, inertia, L)
-    acceleration2 = calculate_auv_angular_acceleration(T[1], T2, inertia, L)
-    acceleration3 = calculate_auv_angular_acceleration(T[2], T3, inertia, L)
-    acceleration4 = calculate_auv_angular_acceleration(T[3], T4, inertia, L)
+    better_l = np.sqrt(L**2 + l**2)
+    acceleration1 = calculate_auv_angular_acceleration(T[0], T1, inertia, better_l)
+    acceleration2 = calculate_auv_angular_acceleration(T[1], T2, inertia, better_l)
+    acceleration3 = calculate_auv_angular_acceleration(T[2], T3, inertia, better_l)
+    acceleration4 = calculate_auv_angular_acceleration(T[3], T4, inertia, better_l)
 
     return acceleration1 + acceleration2 + acceleration3 + acceleration4
+
+
+def simulate_auv2_motion(
+    T, alpha, L, l, mass=100, inertia=100, dt=0.1, t_final=10, x0=0, y0=0, theta0=0
+):
+    """
+    The acceleration remains constant, thus we do not need to integrate for position. We can therefore use the formula
+    S = 1/2 at^2 to calculate the distance traveled in a direction
+
+    The velocity can be calculated by simply increasing from 0 linearly with dt and acceleration
+
+
+    """
+
+    t = np.array([])
+    x = np.array([])
+    y = np.ndarray([])
+    theta = np.array([])
+    v = np.array([])
+    omega = np.array([])
+    a = np.array([])
+
+    acceleration = calculate_auv2_acceleration(T, alpha, theta0, mass)
+    ang_acceleration = calculate_auv2_angular_acceleration(T, alpha, L, l, inertia)
+    for i in range(0, t_final + dt, dt):
+        np.append(t, [i])
+        np.append(x, [x0 + 0.5 * acceleration[0] * (i**2)])
+        np.append(y, [y0 + 0.5 * acceleration[1] * (i**2)])
+        np.append(theta, [theta0 + 0.5 * ang_acceleration * (i**2)])
+        np.append(v, acceleration * i)
+        np.append(omega, ang_acceleration * i)
+        np.append(a, acceleration)
+
+    return (t, x, y, theta, v, omega, a)
+
+
+def plot_auv_2_motion(t, x, y, theta, v, omega, a):
+    fig, ax = matplotlib.subplots()
+    matplotlib.plot(x, y)
+
+    ax.set_xlabel("X position")
+    ax.set_ylabel("Y position")
+    ax.set_title("AUV2 Position Over Time")
+
+    matplotlib.show()
+
+
+print(calculate_auv2_acceleration([10, 1, 4, 0], 0.12, 0.5, 100))
+print(calculate_auv2_angular_acceleration([10, 1, 4, 0], 0.12, 5, 4, 1))
